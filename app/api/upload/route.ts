8b2fa9ff5c2654b2401424f85454713b45ed79a0
fuad1789 <fuadbagiyev@gmail.com>
2026-03-21 02:09:51 +0400

    fixed problem

--- app/api/upload/route.ts
+++ app/api/upload/route.ts
@@ -2,12 +2,6 @@ import { NextResponse } from 'next/server';
 import { writeFile, mkdir } from 'fs/promises';
 import { join } from 'path';
 
-export const config = {
-  api: {
-    bodyParser: false,
-    sizeLimit: '20mb',
-  },
-};
 
 export async function POST(request: Request) {
   try {
